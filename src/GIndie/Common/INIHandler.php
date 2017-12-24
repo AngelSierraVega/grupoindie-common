5d3a355458c67699c0a792d8b42a6386b25ceaa6
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-12-23 19:36:51 -0600

    Proyecto base creado

---
+++ src/GIndie/Common/INIHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * GICommon - INIHandler 2017-12-23
+ * @copyright (C) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package GrupoIndie
+ * @subpackage Common
+ */
+
+namespace GIndie\Common;
+
+/**
+ * Description of INIHandler
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @version GI-CMMN.00.00
+ */
+class INIHandler
+{
+    //put your code here
+}
